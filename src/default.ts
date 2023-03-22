eaf2c412d23c98ea951ec8f748dd4af0ee9c35a1
easonChen <116425753@qq.com>
2023-03-22 09:41:58 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,7 +9,7 @@ export const defaultSetting = {
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 这个应用只对新技术touch,不承担任何使用后涉及政治、法律责任，基于 [chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 开发，欢迎自部署。
-- 如果本项目对你有所帮助，可以给小猫[买条鱼](https://cdn.jsdelivr.net/gh/easonChen007/chatgptForEason@main/assets/reward.gif)，但不接受任何付费功能请求。
+- 如果本项目对你有所帮助，可以给小猫[买](https://cdn.jsdelivr.net/gh/easonChen007/chatgptForEason@main/assets/reward.gif)，但不接受任何付费功能请求。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
 
 
